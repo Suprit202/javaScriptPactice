d72db3fc545d250ca1997f099ede335e3b4cd666
Suprit202 <supritmhnt123@gmail.com>
2025-08-16 21:02:43 +0530

    Add evenOdd.js and signCheck.js for number classification demonstrations

---
+++ src/script/evenOdd.js
@@ -0,0 +1,7 @@
+let a = 9;
+
+if (a%2 == 0) {
+  console.log(`It's a even number`);
+} else {
+  console.log(`It's an odd number`);
+}
---
+++ src/script/signCheck.js
@@ -0,0 +1,12 @@
+let a = 0;
+
+if (a > 0) {
+  console.log(`It's a Positive Number`)
+}
+
+if (a < 0) {
+  console.log(`It's a Negative Number`)
+}
+if (a === 0) {
+  console.log(`It's Zero`)
+}
